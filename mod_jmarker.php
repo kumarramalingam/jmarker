75b192b9281573b56f4f7d13a0071a90d3c7cabc
Kumar Ramalingam <weblogicx@weblogicx3>
2015-03-25 12:26:22 +0530

    -- load map in default view

--- mod_jmarker.php
+++ mod_jmarker.php
@@ -14,4 +14,24 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 require_once __DIR__ . '/helper.php';
 include_once JPATH_LIBRARIES.'/fof/include.php';
 $app = JFactory::getApplication();
+
+/**
+ * @var $markerone Get marker name.
+ */
+$markerone = $params->get('marker_one');
+$markeronelat = $params->get('marker_one_lat');
+$markeronelng = $params->get('marker_one_lng');
+
+$markertwo = $params->get('marker_two');
+$markertwolat = $params->get('marker_two_lat');
+$markertwolng = $params->get('marker_two_lng');
+
+$markerthree = $params->get('marker_three');
+$markerthreelat = $params->get('marker_three_lat');
+$markerthreelng = $params->get('marker_three_lng');
+
+$markerfour = $params->get('marker_four');
+$markerfourlat = $params->get('marker_four_lat');
+$markerfourlng = $params->get('marker_four_lng');
+
 require( JModuleHelper::getLayoutPath('mod_jmarker'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -12,6 +12,42 @@
 // No direct access
 defined('_JEXEC') or die;
 JHtml::_('jquery.framework');
-echo "welcome";
 ?>
-			
+<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+
+<div id="map" style="width: 500px; height: 400px;"></div>
+
+<script type="text/javascript">
+		
+    var locations = [
+      ['<?php echo $markerone ?>','<?php echo $markeronelat ?>','<?php echo $markeronelng ?>' ],
+      ['<?php echo $markertwo ?>','<?php echo $markertwolat ?>','<?php echo $markertwolng ?>'],
+      ['<?php echo $markerthree ?>','<?php echo $markerthreelat ?>','<?php echo $markerthreelng ?>'],
+      ['<?php echo $markerfour ?>','<?php echo $markerfourlat ?>','<?php echo $markerfourlng?>']
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+          
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  </script>

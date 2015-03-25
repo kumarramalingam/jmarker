c1eff2d4ab81cf562c0e9a155811ba25fc5e9185
Kumar Ramalingam <weblogicx@weblogicx3>
2015-03-25 11:36:33 +0530

    -- Initial commit.

---
+++ helper.php
@@ -0,0 +1,25 @@
+<?php
+/*------------------------------------------------------------------------
+# com_jmarker
+# ------------------------------------------------------------------------
+# author    Kumar Ramalingam - http://www.w3cert.in
+# mail      kumar@w3cert.in
+# copyright Copyright (C) 2012 W3cert.in All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://w3cert.in
+-------------------------------------------------------------------------*/
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * JMarker - Helper class.
+ */ 
+class ModJMarkerHelper {	
+	
+	public function getMarkers() {
+				
+	}	
+}
+
+
---
+++ tmpl/default.php
@@ -0,0 +1,17 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_jmarker
+# ------------------------------------------------------------------------
+# author    Kumar Ramalingam - http://www.w3cert.in
+# mail      kumar@w3cert.in
+# copyright Copyright (C) 2012 W3cert.in All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://w3cert.in
+-------------------------------------------------------------------------*/
+
+// No direct access
+defined('_JEXEC') or die;
+JHtml::_('jquery.framework');
+echo "welcome";
+?>
+			
